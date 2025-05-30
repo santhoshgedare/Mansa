6e31c8d4fad9f03da505c9a03f5e43472794be96
Santhosh G <33170603+santhoshgedare@users.noreply.github.com>
2025-05-30 16:28:01 +0530

    Update HomeController.cs

--- Mansa.Web/Controllers/HomeController.cs
+++ Mansa.Web/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Mansa.Web.Controllers
         {
             return View();
         }
+        public IActionResult DataDeletion()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
